3fbd865e46e69b20d5d9d074e233a1fe4f2547b4
Teemone <fynx626@gmail.com>
2021-08-28 15:17:54 +0100

    minor changes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -135,5 +138,5 @@ LOGIN_REDIRECT_URL = 'home'
 # Login redirect url
 LOGOUT_REDIRECT_URL = 'home'
 
-# Activate django-heroku
+# Activate django-heroku  
 django_heroku.settings(locals())
